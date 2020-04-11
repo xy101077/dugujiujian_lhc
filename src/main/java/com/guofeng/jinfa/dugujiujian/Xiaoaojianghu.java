1fd4783db2ff43050a5f8b4cae96ceff30ab1b38
lhc <xy101077>
2020-04-11 13:02:48 +0800

    笑傲江湖:LHC

---
+++ src/main/java/com/guofeng/jinfa/dugujiujian/Xiaoaojianghu.java
@@ -0,0 +1,12 @@
+package com.guofeng.jinfa.dugujiujian;
+
+/**
+ * @Author 王国风
+ * @Date 2020/4/11 13:00
+ * @Version 1.0
+ */
+public class Xiaoaojianghu {
+    public static void main(String[] args) {
+        System.out.println("令狐少侠：沧海一声笑");
+    }
+}
