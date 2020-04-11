f19a843fb476252cdd5e0a8c0964f878df5700b6
lhc <xy101077>
2020-04-11 13:07:17 +0800

    令狐冲提交1

---
+++ src/main/java/com/guofeng/jinfa/dugujiujian/DongFang.java
@@ -0,0 +1,12 @@
+package com.guofeng.jinfa.dugujiujian;
+
+/**
+ * @Author 王国风
+ * @Date 2020/4/11 13:05
+ * @Version 1.0
+ */
+public class DongFang {
+    public static void main(String[] args) {
+        System.out.println("东方不亮西方亮");
+    }
+}
