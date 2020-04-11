39fb0bb9fb77daa0afebfd1d4fd2601bf418c68e
xy101077 <1741201002@qq.com>
2020-04-11 10:36:05 +0800

    Initial commit

---
+++ src/main/java/com/guofeng/jinfa/dugujiujian/Dugu.java
@@ -0,0 +1,12 @@
+package com.guofeng.jinfa.dugujiujian;
+
+/**
+ * @Author 王国风
+ * @Date 2020/4/11 9:50
+ * @Version 1.0
+ */
+public class Dugu {
+    public static void main(String[] args) {
+        System.out.println("jianfa dugujiujian!!!");
+    }
+}
