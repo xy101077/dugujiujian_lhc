0b4781a104039cd0741b0113ff503c1a6a2f4129
lihuchong <xy101077>
2020-04-11 12:41:32 +0800

    xinjianfa

---
+++ src/main/java/com/guofeng/jinfa/dugujiujian/Newjianfa.java
@@ -0,0 +1,12 @@
+package com.guofeng.jinfa.dugujiujian;
+
+/**
+ * @Author 王国风
+ * @Date 2020/4/11 12:37
+ * @Version 1.0
+ */
+public class Newjianfa {
+    public static void main(String[] args) {
+        System.out.println("新剑法");
+    }
+}
