f6b091bbb943eb886d1f7f0a202e8a3b8e681ea2
yuebuqun <gf101077>
2020-04-11 11:24:25 +0800

    岳不群修改

--- src/main/java/com/guofeng/jinfa/dugujiujian/Dugu.java
+++ src/main/java/com/guofeng/jinfa/dugujiujian/Dugu.java
@@ -8,5 +8,8 @@ package com.guofeng.jinfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("jianfa dugujiujian!!!");
+        System.out.println("jianfa dugujiujian!!!");
+        System.out.println("jianfa dugujiujian!!!");
+        System.out.println("jianfa dugujiujian!!!");
     }
 }
